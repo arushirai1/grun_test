f45b5e37fbfcb311c0205ab141294a823f23f5f5
Arushi Rai <arushirai1999@gmail.com>
2020-08-19 22:13:54 -0500

    Initial commit

--- index.py
+++ index.py
@@ -1,24 +1,17 @@
 import os
-import shutil
 import requests
-import tempfile
+
 
 from gevent.pywsgi import WSGIServer
 from flask import Flask, after_this_request, render_template, request, send_file
 
-UPLOAD_FOLDER = '/tmp'
-ALLOWED_EXTENSIONS = set(['doc', 'docx', 'xls', 'xlsx'])
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def api():
-    if request.method == 'GET':
-        return "Hello World"
- 
-#return send_file(output_file_path, mimetype='application/pdf')
-
+    return "Hello World"
 
 if __name__ == "__main__":
     http_server = WSGIServer(('', int(os.environ.get('PORT', 8080))), app)
